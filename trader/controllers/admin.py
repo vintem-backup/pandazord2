60a062229084a27d514f48e03cd1740780ac151c
marcusmello <mandealista@gmail.com>
2020-02-08 23:44:04 -0300

    Changed the table and some attributes names

--- trader/controllers/admin.py
+++ trader/controllers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BinanceAssets
+from .models import BinanceAsset
 
 # Register your models here.
-admin.site.register(BinanceAssets)
+admin.site.register(BinanceAsset)
