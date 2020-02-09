1e30577a6ca09214dc56e7358a21e8fc3d0989f6
marcusmello <mandealista@gmail.com>
2020-02-08 23:44:21 -0300

    Changed the table and some attributes names

--- trader/controllers/models.py
+++ trader/controllers/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
-class BinanceAssets (models.Model):
+class BinanceAsset (models.Model):
 
     class Meta:
 
         db_table = '"binance_assets"'
     
-    STATUS_CHOICES=(
+    AUTO_UPDATE_CHOICES=(
         ('ON','on'),
         ('OFF','off'),
         )
 
     asset_symbol = models.CharField(max_length=8, primary_key=True)
-    get_data = models.CharField(max_length=3, choices=STATUS_CHOICES, default='OFF')
+    auto_update = models.CharField(max_length=3, choices=AUTO_UPDATE_CHOICES, default='OFF')
     status = models.CharField(max_length=8, default='absent')
-    last_change_by_pid = models.IntegerField(null=True, blank=True)
+    last_modified_by = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.asset_symbol
