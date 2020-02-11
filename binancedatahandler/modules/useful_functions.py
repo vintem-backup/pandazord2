f68e89bdf1125679a80f0d077f67b2976ed9ac8b
marcusmello <mandealista@gmail.com>
2020-02-11 15:09:41 -0300

    New modules directory

--- binancedatahandler/modules/useful_functions.py
+++ binancedatahandler/modules/useful_functions.py
@@ -1,6 +1,12 @@
 #TODO: Docstrings and type annotations
+
 from datetime import datetime
 import requests
+import os
+from copy import deepcopy
+
+oldest_open_time_in_milliseconds = 1241893500000
+#oldest_open_time_in_milliseconds = os.environ['oldest_open_time_in_milliseconds']
 
 def utc_time_func():
     
@@ -25,7 +31,8 @@ def utc_time_func():
 
     return utc_now
 
-def delta_time_in_exact_seconds_from_hours_between_utc_and(this_time):
+
+def delta_time_in_seconds_rounded_from_integers_hours_between_utc_and(this_time):
 
     utc_time = utc_time_func()
     
@@ -36,3 +43,84 @@ def delta_time_in_exact_seconds_from_hours_between_utc_and(this_time):
     delta = delta_hour*3600
     
     return delta
+
+
+def format_klines(raw_klines, delta):
+    
+    def clear_columns_and_adjust_time_and_prices_and_volume_on(raw_klines, delta):
+
+        klines_out = []
+
+        for i in range (len(raw_klines)):
+
+            data = [datetime.fromtimestamp(int(raw_klines[i][0]/1000) + delta), #open_time
+                    float(raw_klines[i][1]), #Open
+                    float(raw_klines[i][2]), #High
+                    float(raw_klines[i][3]), #Low
+                    float(raw_klines[i][4]), #Close
+                    float(raw_klines[i][5]) #volume
+                    ]
+
+            klines_out.append(data)
+
+        return klines_out
+
+    def making_seconds_be_zero_on(klines_in):
+
+        klines_out = deepcopy(klines_in)
+
+        for i in range(len(klines_in)):
+
+            entry = klines_in[i][0]
+
+            if (entry.second != 0):
+
+                out = datetime.fromtimestamp((datetime.timestamp(entry) - entry.second))
+
+                klines_out[i][0] = out
+
+        return klines_out
+
+    klines_adjusted = clear_columns_and_adjust_time_and_prices_and_volume_on(raw_klines, delta)
+
+    klines = making_seconds_be_zero_on(klines_adjusted)
+
+    return klines
+
+
+def replace_with_zero_where_data_is_missing(last_open_time, klines):
+    
+    oldest_open_time = bool(last_open_time == oldest_open_time_in_milliseconds)
+    
+    if (oldest_open_time): klines_out = klines
+    
+    else:
+        
+        auxiliary_list = []
+    
+        auxiliary_list.append(int(last_open_time + 60000))
+
+        for i in range(0, len(klines)-1):
+
+            auxiliary_list.append(auxiliary_list[i] + 60000)
+    
+        klines_out = deepcopy(klines)
+
+        for i in range (len(klines)):
+
+            compatibility = bool(int(klines_out[i][0]) == auxiliary_list[i])
+
+            if (compatibility): pass
+
+            else:
+
+                klines_out[i][0] = auxiliary_list[i] #Open_time
+                klines_out[i][1] = 0.0 #Open
+                klines_out[i][2] = 0.0 #High
+                klines_out[i][3] = 0.0 #Low
+                klines_out[i][4] = 0.0 #Close
+                klines_out[i][5] = 0.0 #volume
+
+    last_open_time = int((klines_out[len(klines_out)-1][0]))
+    
+    return last_open_time, klines_out
