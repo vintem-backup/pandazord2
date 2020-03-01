0e65c95d780f8fb2ac646fc470397839c9424989
marcusmello <mandealista@gmail.com>
2020-03-01 18:27:35 -0300

    Stop_loss draft. Only 'how_many_candles' method finished

--- trader/modules/stop_loss.py
+++ trader/modules/stop_loss.py
@@ -1,7 +1,39 @@
-class Default:
-    
-    def __init__(self, operational_parameters):
-        
-        pass
+from .common_libs import PriceSeriesFrom
 
-    pass
+class Default:
+
+    class Trigger:
+        
+        def __init__(self, parameters):
+            
+            self.rate = parameters['rate(%)']/100
+            self.n_measurements = parameters['treshold']['n_measurements']
+            self.n_positives = parameters['treshold']['n_positives']
+    
+    def __init__(self, parameters):
+
+        self.price_source = parameters['price_source']
+        self.first, self.second, self.update = (self.Trigger(parameters['first_trigger']), 
+        self.Trigger(parameters['second_trigger']), 
+        self.Trigger(parameters['update_target_if']))
+
+        
+    def how_many_candles(self): #ok
+        
+        return max(self.first.n_measurements, self.second.n_measurements, self.update.n_measurements)
+    
+
+    def is_stop(self, klines): #not_ok
+        
+        price = getattr(PriceSeriesFrom(klines), price_source + '_')()
+    
+    
+    def verify(self, klines, position): #not_ok
+
+        class Stop: #not_ok
+            
+            def __init__(self, klines): #not_ok
+                
+                self.is_true = self.is_stop(klines)
+        
+        return Stop(klines)
